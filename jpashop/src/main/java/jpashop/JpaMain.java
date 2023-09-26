33078bb3c951b861816b8597e01c2309bd9b4e83
HEEJIN <heehee2310@gmail.com>
2023-09-27 07:53:32 +0900

    실전예제5 - 프록시, 연관관계 관리

--- jpashop/src/main/java/jpashop/JpaMain.java
+++ jpashop/src/main/java/jpashop/JpaMain.java
@@ -1,8 +1,55 @@
 package jpashop;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import jpashop.model.entity.Delivery;
+import jpashop.model.entity.Order;
+import jpashop.model.entity.OrderItem;
+
 public class JpaMain {
 
     public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+//        notUseTransitivePersistence(em);
+        useTransitivePersistence(em);
+        tx.commit();
 
     }
+
+    public static void notUseTransitivePersistence(EntityManager em) {
+        Delivery delivery = new Delivery();
+        em.persist(delivery);
+
+        OrderItem orderItem1 = new OrderItem();
+        OrderItem orderItem2 = new OrderItem();
+        em.persist(orderItem1);
+        em.persist(orderItem2);
+
+        Order order = new Order();
+        order.setDelivery(delivery);
+        order.addOrderItem(orderItem1);
+        order.addOrderItem(orderItem2);
+
+        em.persist(order);
+    }
+
+    public static void useTransitivePersistence(EntityManager em) {
+        Delivery delivery = new Delivery();
+        OrderItem orderItem1 = new OrderItem();
+        OrderItem orderItem2 = new OrderItem();
+
+        Order order = new Order();
+        order.setDelivery(delivery);
+        order.addOrderItem(orderItem1);
+        order.addOrderItem(orderItem2);
+
+        em.persist(order);
+    }
+
 }
--- jpashop/src/main/java/jpashop/model/entity/Order.java
+++ jpashop/src/main/java/jpashop/model/entity/Order.java
@@ -3,10 +3,12 @@ package jpashop.model.entity;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -35,14 +37,16 @@ public class Order extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order"
+        , cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY
+        , cascade = CascadeType.ALL)
     @JoinColumn(name = "DELIVERY_ID")
     private Delivery delivery;
 
